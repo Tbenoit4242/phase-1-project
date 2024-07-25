011e5a7a5354f3a8ea66761a823f2200005a6949
Trevor Benoit <Trevorb042@gmail.com>
2024-07-25 12:45:48 -0600

    working fetch

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-document.addEventListener("DOMContentLoaded", () => console.log("we are connected!!!"))
+document.addEventListener("DOMContentLoaded", () => fetchData())
+
+function fetchData(){
+    fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita")
+    .then((resp) => resp.json())
+    .then((data) => console.log(data))
+}
