47a2f14dba4c5ed09d2bfb70126e469afb448ab2
Trevor Benoit <Trevorb042@gmail.com>
2024-07-27 12:11:50 -0600

    fetch setup

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-document.addEventListener("DOMContentLoaded", () => fetchData())
+document.addEventListener("DOMContentLoaded", () => {
+    fetchData();
+});
 
-function fetchData(){
+function fetchData() {
     fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita")
-    .then((resp) => resp.json())
-    .then((data) => console.log(data))
+        .then((resp) => resp.json())
+        .then((data) => {
+            console.log(data)
+            displayDrinks(data.drinks);
+        });
 }
