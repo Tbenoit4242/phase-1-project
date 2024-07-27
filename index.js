b58718fa6e3be02ffddf5c0c94d1524895da09d8
Trevor Benoit <Trevorb042@gmail.com>
2024-07-27 13:02:54 -0600

    drink list

--- index.js
+++ index.js
@@ -15,3 +15,4 @@ function fetchData() {
 // Select the input, button, and various divs from the DOM
 const searchInput = document.getElementById('searchInput');
 const searchButton = document.getElementById('searchButton');
+const drinkList = document.getElementById('drinkList');
