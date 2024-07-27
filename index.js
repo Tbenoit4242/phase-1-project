d52b9ef8fda5fb2a130619e2c831076041c412b4
Trevor Benoit <Trevorb042@gmail.com>
2024-07-27 13:00:43 -0600

    adding search input

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
+// This event listener ensures the initial data fetch happens after the DOM content is fully loaded
 document.addEventListener("DOMContentLoaded", () => {
     fetchData();
 });
 
+// Function to fetch initial data from the API
 function fetchData() {
     fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita")
         .then((resp) => resp.json())
         .then((data) => {
-            console.log(data)
-            displayDrinks(data.drinks);
+            displayDrinks(data.drinks);  // Call to display the fetched drinks
         });
 }
+
+// Select the input, button, and various divs from the DOM
+const searchInput = document.getElementById('searchInput');
